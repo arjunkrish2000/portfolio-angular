4e26b4dd5ce54845b719111bec2c7ef7bdaf9845
Arjun Krishna J G <44795912+arjunkrish2000@users.noreply.github.com>
2023-01-31 13:45:37 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcademicsComponent } from './academics/academics.component';
+import { AddressComponent } from './address/address.component';
+import { AppModule } from './app.module';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path:'home',component:HomeComponent},
+  {path:'address',component:AddressComponent},
+    {path:'academics',component:AcademicsComponent},
+     {path:'project',component:ProjectComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddressComponent } from './address/address.component';
+import { ProjectComponent } from './project/project.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcademicsComponent,
+    NavbarComponent,
+    AddressComponent,
+    ProjectComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
